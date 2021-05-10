0a29360d8be8e158b732ed3529661dea7c43c32f
Ryan Hornberger <ryan.hornberger@mywhym.co>
2021-05-10 10:24:37 -0400

    Improving the packaging

---
+++ src/graphql/index.ts
@@ -0,0 +1,4 @@
+export { default as buildGqlMiddleware } from "./buildGqlMiddleware"
+export * as commonSchema from "./commonSchema"
+export { default as compileGqlSchema } from "./compileGqlSchema"
+export { default as executeGqlQuery } from "./executeGqlQuery"
